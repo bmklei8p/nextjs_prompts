dd06d089d8c2ebb5a9ee5fce8b7c0effcbbd8e63
bryan <bmklei8p@gmail.com>
2023-06-24 12:00:33 -0400

    create prompt form complete

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,8 +3,6 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 import { connectToDB } from '@utils/database'
 
-import User from "@models/user";
-import { connectToDB } from "@utils/database";
 
 connectToDB();
 
@@ -40,51 +38,3 @@ const handler = NextAuth({
 })
 
 export { handler as GET, handler as POST }
-
-
-
-
-
-// import NextAuth from "next-auth/next";
-// import GoogleProvider from 'next-auth/providers/google'
-
-// import User from "@models/user";
-// import { connectToDB } from "@utils/database";
-
-// const handler = NextAuth({
-//     providers: [
-//         GoogleProvider({
-//             clientId: process.env.GOOGLE_ID,
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//         })
-//     ],
-//     callbacks: {
-//     async session({ session }) {
-//         const sessionUser = await User.findOne({ email: session.user.email})
-//         session.user.id = sessionUser._id.toString();
-//         return session;
-//     },
-//     async signIn({ profile }) {
-//         try {
-//             // serverless but that means it has to spin up connection to db each time
-//             // you hit it
-//             await connectToDB();
-//             // check if user already exits
-//             const userExists = await User.findOne({ email: profile.email })
-//             // if not, create a new user
-//             if (!userExists) {
-//                 await User.create({
-//                     email: profile.email,
-//                     username: profile.name.replace(" ", "").toLowerCase(),
-//                     image: profile.picture,
-//                 })
-//             }
-//         } catch (error) {
-//             console.log(error)
-//             return false;
-//         }
-//         }
-//     }
-// })
-
-// export { handler as GET, handler as POST }
