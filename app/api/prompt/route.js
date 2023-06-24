fc1c4b529d15402bc78aee46934d2ef7776c21db
bryan <bmklei8p@gmail.com>
2023-06-24 16:19:48 -0400

    added prompt connect to db

---
+++ app/api/prompt/route.js
@@ -0,0 +1,15 @@
+import { connectToDB } from "@utils/database"
+import Prompt from "@models/prompt";
+
+connectToDB();
+
+export const GET = async (req) => {
+    try {
+        const prompts = await Prompt.find({}).populate("creator");
+        console.log(prompts)
+        return new Response(JSON.stringify(prompts), {status: 200})
+    } catch(error) {
+        console.log(error)
+        return new Response("Failed to get prompts", {status: 500})
+    }
+}
